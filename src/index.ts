ad04310d644da2ea9a1f115fa42860728ec40e73
Noreen <chen0901037187@gmail.com>
2024-04-10 19:14:44 +0800

    feat(ts): 利用陣列紀錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,23 @@
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 10)
-);
-console.log(product);
+const products = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+  new Product(
+    2,
+    "金魚都能懂的 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10)
+  ),
+];
 
-let productId: number;
-productId = 1;
-console.log("Product Id = " + productId);
-
-const productName = "圖像 Angular 開發入門 第二版";
-console.log("Product Name = " + productName);
-
-let isShow = true;
-console.log("Is Show = " + isShow);
-
-let createDate = new Date();
-console.log("Create Date", createDate);
-
-let modifyDate: Date | undefined = undefined;
-console.log("Modify Date", modifyDate);
-
-console.log(product);
-console.table(product);
+console.log(products);
+console.table(products);
