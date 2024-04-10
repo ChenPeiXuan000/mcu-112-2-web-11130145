eff1e3a8259adf915a238ce517a4e9592b3f018a
Noreen <chen0901037187@gmail.com>
2024-04-10 18:24:10 +0800

    feat(ts): 利用物件紀錄產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+const product = {
+  id: 1,
+  name: "圖像 Angular 開發入門 第二版",
+  company: "博碩文化",
+  price: 500,
+  isShow: true,
+  createDate: new Date(2024, 3, 10),
+  modifyDate: undefined,
+};
+
 let productId: number;
 productId = 1;
 console.log("Product Id = " + productId);
@@ -13,3 +23,6 @@ console.log("Create Date", createDate);
 
 let modifyDate: Date | undefined = undefined;
 console.log("Modify Date", modifyDate);
+
+console.log(product);
+console.table(product);
