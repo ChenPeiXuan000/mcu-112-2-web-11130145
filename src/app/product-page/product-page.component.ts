3d7cf9f3cc392edf66b336961a2de4d3d01273ee
Noreen <chen0901037187@gmail.com>
2024-05-30 17:17:08 +0800

    feat(product): 產品資料查詢與分頁實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,7 +2,7 @@ import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subject, startWith, switchMap } from 'rxjs';
+import { BehaviorSubject, Subject, combineLatest, startWith, switchMap, tap } from 'rxjs';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { ProductService } from './../services/product.service';
@@ -17,25 +17,52 @@ import { ProductService } from './../services/product.service';
 export class ProductPageComponent {
   router = inject(Router);
 
-  private ProductService = inject(ProductService);
+  private productService = inject(ProductService);
 
   protected pageSize = 5;
 
   private readonly refresh$ = new Subject<void>();
 
-  protected readonly formControl = new FormControl<string | undefined>(undefined);
+  protected readonly formControl = new FormControl<string | undefined>(undefined, { nonNullable: true });
 
-  pageIndex = 1;
+  private readonly condition$ = new BehaviorSubject<string | undefined>(undefined);
+  get condition() {
+    return this.condition$.value;
+  }
+  set condition(value: string | undefined) {
+    this.condition$.next(value);
+  }
 
-  readonly products$ = this.refresh$.pipe(
-    startWith(undefined),
-    switchMap(() => this.ProductService.getList(undefined, 1, 5))
+  private readonly pageIndex$ = new BehaviorSubject<number>(1);
+  get pageIndex() {
+    return this.pageIndex$.value;
+  }
+
+  set pageIndex(value: number) {
+    this.pageIndex$.next(value);
+  }
+
+  readonly products$ = combineLatest([
+    this.refresh$.pipe(
+      startWith(undefined),
+      tap((condition) => console.log('refresh', condition))
+    ),
+    this.condition$.pipe(tap((condition) => console.log('condition', condition))),
+    this.pageIndex$.pipe(tap((Index) => console.log('pageIndex', Index))),
+  ]).pipe(
+    tap((data) => console.log(data)),
+    switchMap(([_, condition, pageIndex]) => this.productService.getList(condition, pageIndex, this.pageSize)),
+    tap((data) => console.log(data))
   );
 
-  readonly totalCount$ = this.refresh$.pipe(
-    startWith(undefined),
-    switchMap(() => this.ProductService.getCount())
+  readonly totalCount$ = combineLatest([this.refresh$.pipe(startWith(undefined)), this.condition$]).pipe(
+    switchMap(([_, condition]) => this.productService.getCount(condition))
   );
+
+  onPageIndexChange(index: number): void {
+    console.log(index);
+    this.pageIndex = index;
+  }
 
   onAdd(): void {
     const product = new Product({
@@ -47,7 +74,7 @@ export class ProductPageComponent {
       createDate: new Date(),
       price: 10000,
     });
-    this.ProductService.add(product).subscribe(() => this.refresh$.next());
+    this.productService.add(product).subscribe(() => this.refresh$.next());
   }
 
   onEdit(product: Product): void {
@@ -55,7 +82,7 @@ export class ProductPageComponent {
   }
 
   onRemove({ id }: Product): void {
-    this.ProductService.remove(id).subscribe(() => this.refresh$.next());
+    this.productService.remove(id).subscribe(() => this.refresh$.next());
   }
 
   onView(product: Product): void {
